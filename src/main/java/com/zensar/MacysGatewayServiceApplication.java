5e2574ee1516e09786e58825acd83c688fed8171
Harshit <srivastavaharshit008@gmail.com>
2022-01-12 18:21:35 +0530

    Initial commit

---
+++ src/main/java/com/zensar/MacysGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacysGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacysGatewayServiceApplication.class, args);
+	}
+
+}
